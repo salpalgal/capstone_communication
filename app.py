cb7c29d4f2088294dac9bc578ef3d07b346afb29
salpalgal <kusos6395@gmail.com>
2025-02-23 19:51:11 +0800

    - at app.py line 27

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///communication_db'))
--
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
